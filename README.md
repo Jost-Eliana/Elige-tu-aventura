7bbbe163e07c9b081286ff8a49f1a77b7d5b82cc
Jost-Eliana <Elianajost@outlook.com>
2022-04-03 23:53:15 -0300

    Elige-tu-aventura

---
+++ README.md
@@ -0,0 +1,8 @@
+# Crazy Colors
+
+En este ejercicio tendras que refactorizar el codigo dentro de App.jsx. En el mismo deberan implementar lo aprendido del state y setState
+
+### Pasos para correr el proyecto
+
+Relizar primero `npm install`
+Relizar segundo `npm start`
---
+++ src/App.jsx
@@ -0,0 +1,33 @@
+import React, { Component } from 'react'
+import Button from './Button'
+import "./App.css";
+
+export default class App extends Component {
+  colors = ["RED","BLUE","YELLOW", "GREEN","ORANGE","MAGENTA","BROWN","LIME"]
+  /* ESPACIO DE TRABAJO
+  - Armar la estructura para una Class Component
+  - Hacer un state con el arreglo de los colores
+  - Vamos a usar una funcion shuffle mas abajo y vamos a enviarla por props... que
+    formas tenemos para no perder el contexto?
+  */
+
+
+  /* ESPACIO DE TRABAJO
+  Funcion Shuffle
+  Aqui deberan hacer una funcion que me retorne un nuevo arreglo con los
+  colores mezclados
+  */
+
+  
+  render() {
+    return (
+      <div className="container">
+        <div className="panel">
+          {this.colors.map(color => <div key={color} className={color}>{color}</div>)}
+        </div>
+        <Button fn={"Recibe una funcion"} />
+      </div>
+
+    )
+  }
+}
